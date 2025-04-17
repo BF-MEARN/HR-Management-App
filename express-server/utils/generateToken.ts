42225b2ed8191e63c61169f26932d299d02a679f
Daniel Ho <daniel.ho337@gmail.com>
2025-04-17 12:26:06 -0700

    Add AuthMiddleWare along with generateSecretJWT & generateToken

---
+++ express-server/utils/generateToken.ts
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+interface User {
+    _id: string,
+    username: string,
+    password: string,
+    email: string,
+    role: string,
+    isActive: boolean,
+    isCreated: boolean,
+}
+
+const generateToken = (user: User) => {
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username,
+            password: user.password,
+            email: user.email,
+            role: user.role,
+            isActive: user.isActive,
+            isCreated: user.isCreated,
+        },
+        process.env.JWT_SECRET!
+    )
+
+    return token;
+}
+
+export default generateToken;
