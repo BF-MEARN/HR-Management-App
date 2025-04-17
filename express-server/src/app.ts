f35a97e9b6ad45e84318cb3652bfbc1d92fd2624
Justin Case <justin.case1061@gmail.com>
2025-04-17 13:26:56 -0500

    changed file structure, added npm commands, fixed bug, commented out stray route handler

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -7,9 +7,9 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
-app.all('*', (_req, res) => {
-  res.status(404).json({ message: 'Not Found' });
-});
+// app.all('*', (_req, res) => {
+//   res.status(404).json({ message: 'Not Found' });
+// });
 
 
 export default app
