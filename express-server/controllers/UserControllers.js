12ca004111ec8ef5cb09b98bd17d4cc482e675a7
Justin Case <justin.case1061@gmail.com>
2025-04-18 12:30:27 -0500

    removed comment

--- express-server/controllers/UserControllers.js
+++ express-server/controllers/UserControllers.js
@@ -3,32 +3,6 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import { generateToken } from '../utils/generateToken.js';
 
-
-// export const signup = async (req, res) => {
-//   try {
-
-//     const { username, email, password } = req.body;
-
-//     //look for existing user
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ message: "email already exists" })
-//     }
-
-//     //hash password
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     //save user
-//     const newUser = User.create({ username, email, password: hashedPassword,  })
-
-
-//     res.status(200).json({ message: "user created successfully" })
-
-//   } catch (error) {
-//     res.status(501).json({ message: "server error: ", error })
-//   }
-// }
-
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
