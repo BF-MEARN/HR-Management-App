15d67a580c0c8a5af5eaeb5e166f8abfcb0f50be
Hongzhou Fang <hf92@drexel.edu>
2025-04-17 21:47:31 -0400

    HRP-75: Added basic registration and login page - Both pages use the new AuthForm component - Basic Material UI style - Basic validation in AuthForm - No real functionalities implemented

---
+++ employee-portal/src/pages/Login.tsx
@@ -0,0 +1,30 @@
+import { Card, Typography } from '@mui/material';
+import AuthForm, { AuthFormData } from '../components/AuthForm';
+
+export default function LoginPage() {
+  const handleSubmit = (form: AuthFormData) => {
+    console.log(form);
+  };
+
+  return (
+    <Card
+      variant="outlined"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignSelf: 'center',
+        width: '100%',
+        padding: '2rem',
+        gap: '1rem',
+        marginBlock: '10rem',
+        marginInline: 'auto',
+        maxWidth: '450px',
+      }}
+    >
+      <Typography component="h1" variant="h4" fontWeight={'bold'}>
+        Log in
+      </Typography>
+      <AuthForm onSubmit={handleSubmit} includeEmailField={false} />
+    </Card>
+  );
+}
---
+++ employee-portal/src/pages/Register.tsx
@@ -0,0 +1,30 @@
+import { Card, Typography } from '@mui/material';
+import AuthForm, { AuthFormData } from '../components/AuthForm';
+
+export default function RegisterPage() {
+  const handleSubmit = (form: AuthFormData) => {
+    console.log(form);
+  };
+
+  return (
+    <Card
+      variant="outlined"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignSelf: 'center',
+        width: '100%',
+        padding: '2rem',
+        gap: '1rem',
+        marginBlock: '10rem',
+        marginInline: 'auto',
+        maxWidth: '450px',
+      }}
+    >
+      <Typography component="h1" variant="h4">
+        Register
+      </Typography>
+      <AuthForm onSubmit={handleSubmit} includeEmailField={true} />
+    </Card>
+  );
+}
