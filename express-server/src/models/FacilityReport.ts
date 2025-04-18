54e3687cb0b86579621a266cd09ed5b2e2a6848d
Albert Negoro <albert.negoro89@gmail.com>
2025-04-17 18:42:59 -0700

    fixed import/export styles to ESM

--- express-server/src/models/FacilityReport.ts
+++ express-server/src/models/FacilityReport.ts
@@ -3,31 +3,31 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const FacilityReportSchema = new Schema(
-  {
-    employeeId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee", index: true },
-    houseId: { type: mongoose.Schema.Types.ObjectId, ref: "Housing", index: true },
-    title: { type: String },
-    description: { type: String },
-    status: {
-      type: String,
-      enum: ["Open", "In Progress", "Closed"],
-      default: "Open",
-    },
-    comments: [
-      {
-        createdBy: {
-          type: mongoose.Schema.Types.ObjectId,
-          // Changed ref from FacilityReport to User
-          ref: "User",
-        },
-        description: { type: String },
-        timestamp: { type: Date },
-      },
-    ],
-  },
-  { timestamps: true }
+	{
+		employeeId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee", index: true },
+		houseId: { type: mongoose.Schema.Types.ObjectId, ref: "Housing", index: true },
+		title: { type: String },
+		description: { type: String },
+		status: {
+			type: String,
+			enum: ["Open", "In Progress", "Closed"],
+			default: "Open",
+		},
+		comments: [
+			{
+				createdBy: {
+					type: mongoose.Schema.Types.ObjectId,
+					// Changed ref from FacilityReport to User
+					ref: "User",
+				},
+				description: { type: String },
+				timestamp: { type: Date },
+			},
+		],
+	},
+	{ timestamps: true }
 );
 
 const FacilityReport = mongoose.model("FacilityReport", FacilityReportSchema);
 
-module.exports = FacilityReport;
+export default FacilityReport;
--- express-server/src/models/VisaStatus.ts
+++ express-server/src/models/VisaStatus.ts
@@ -3,61 +3,61 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const VisaStatusSchema = new Schema(
-  {
-    employeeId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Employee",
-      index: true,
-    },
-    workAuthorization: {
-      type: {
-        type: String,
-        enum: ["Green Card", "Citizen", "H1-B", "L2", "F1", "H4", "Other"],
-      },
-      startDate: { type: Date },
-      endDate: { type: Date },
-      otherTitle: { type: String },
-    },
-    optReceipt: {
-      file: { type: String },
-      status: {
-        type: String,
-        enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
-        default: "Not Uploaded",
-      },
-      feedback: { type: String },
-    },
-    optEAD: {
-      file: { type: String },
-      status: {
-        type: String,
-        enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
-        default: "Not Uploaded",
-      },
-      feedback: { type: String },
-    },
-    i983: {
-      file: { type: String },
-      status: {
-        type: String,
-        enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
-        default: "Not Uploaded",
-      },
-      feedback: { type: String },
-    },
-    i20: {
-      file: { type: String },
-      status: {
-        type: String,
-        enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
-        default: "Not Uploaded",
-      },
-      feedback: { type: String },
-    },
-  },
-  { timestamps: true }
+	{
+		employeeId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Employee",
+			index: true,
+		},
+		workAuthorization: {
+			type: {
+				type: String,
+				enum: ["Green Card", "Citizen", "H1-B", "L2", "F1", "H4", "Other"],
+			},
+			startDate: { type: Date },
+			endDate: { type: Date },
+			otherTitle: { type: String },
+		},
+		optReceipt: {
+			file: { type: String },
+			status: {
+				type: String,
+				enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
+				default: "Not Uploaded",
+			},
+			feedback: { type: String },
+		},
+		optEAD: {
+			file: { type: String },
+			status: {
+				type: String,
+				enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
+				default: "Not Uploaded",
+			},
+			feedback: { type: String },
+		},
+		i983: {
+			file: { type: String },
+			status: {
+				type: String,
+				enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
+				default: "Not Uploaded",
+			},
+			feedback: { type: String },
+		},
+		i20: {
+			file: { type: String },
+			status: {
+				type: String,
+				enum: ["Not Uploaded", "Pending Approval", "Approved", "Rejected"],
+				default: "Not Uploaded",
+			},
+			feedback: { type: String },
+		},
+	},
+	{ timestamps: true }
 );
 
 const VisaStatus = mongoose.model("VisaStatus", VisaStatusSchema);
 
-module.exports = VisaStatus;
+export default VisaStatus;
