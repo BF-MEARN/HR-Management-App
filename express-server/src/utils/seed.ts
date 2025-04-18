6a826f6f8ac6a3f15cee5f58c9e606d7edc1caa6
daniellin162 <lindaniel500@gmail.com>
2025-04-17 21:05:35 -0500

    Add seeding for employees

--- express-server/src/utils/seed.ts
+++ express-server/src/utils/seed.ts
@@ -12,6 +12,7 @@ import VisaStatus from "../models/VisaStatus";
 
 const SALT_ROUNDS: number = 10;
 const amountHousing: number = 5;
+const amountUserEmployee: number = 10;
 
 const seedDatabase = async () => {
   console.log("Connecting to database...");
@@ -65,10 +66,61 @@ const seedDatabase = async () => {
     const createdHousing = await Housing.insertMany(housingData);
     console.log(`${createdHousing.length} Housing documents created.`);
 
+    // Seeding Employees and Users
+    console.log("Seeding Employees and Users...");
+    const createdEmployees = [];
+    const createdUsers = [];
+    const employeeUpdates = [];
+    const housingUpdates = [];
+
+    for (let i = 0; i < amountUserEmployee; i++) {
+      const firstName = faker.person.firstName();
+      const lastName = faker.person.lastName();
+      const email = faker.internet.email();
+
+      const newEmployee = new Employee({
+        firstName,
+        lastName,
+        ssn: `${faker.number.int({ min: 100, max: 999 })}-${faker.number.int({
+          min: 10,
+          max: 99,
+        })}-${faker.number.int({ min: 1000, max: 9999 })}`,
+        dob: faker.date.birthdate(),
+        gender: faker.person.sex(),
+        isCitizenOrPR: faker.datatype.boolean(0.3),
+        contactInfo: {
+          cellPhone: faker.phone.number(),
+        },
+        emergencyContacts: [{
+          firstName: faker.person.firstName(),
+          lastName: faker.person.lastName(),
+          phone: faker.phone.number(),
+          email: faker.internet.email(),
+          relationship: faker.helpers.arrayElement(["Parent", "Sibling", "Friend", "Spouse"])
+        }],
+        address: {
+          street: faker.location.streetAddress(),
+          city: faker.location.city(),
+          state: faker.location.state({ abbreviated: true }),
+          zip: faker.location.zipCode(),
+        },
+      });
+      await newEmployee.save();
+      createdEmployees.push(newEmployee);
+    }
+
+    console.log(
+      `${createdEmployees.length} Employees and ${createdUsers.length} Users created.`
+    );
+
     console.log("Closing database connection...");
     await mongoose.connection.close();
     console.log("Database connection closed.");
-  } catch (error) {}
+  } catch (error: unknown) {
+    if (error) {
+      console.error(error);
+    }
+  }
 };
 
 seedDatabase();
