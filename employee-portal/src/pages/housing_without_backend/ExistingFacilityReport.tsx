9368ceb97f6d2924e57ab04e3cbbde239650b1d4
Albert Negoro <albert.negoro89@gmail.com>
2025-04-19 11:18:25 -0700

    chore: fix formatting to pass formatter check

--- employee-portal/src/pages/housing_without_backend/ExistingFacilityReport.tsx
+++ employee-portal/src/pages/housing_without_backend/ExistingFacilityReport.tsx
@@ -1,95 +1,88 @@
-import { memo, useState } from "react"
-import { Button, TextField } from "@mui/material";
-import { Report } from "./Housing";
+import { memo, useState } from 'react';
+
+import { Button, TextField } from '@mui/material';
+
+import { Report } from './Housing';
 
 interface ExistingFacilityReportProps {
-    index: number;
-    title: string;
-    description: string;
-    setReports: React.Dispatch<React.SetStateAction<Report[]>>;
+  index: number;
+  title: string;
+  description: string;
+  setReports: React.Dispatch<React.SetStateAction<Report[]>>;
 }
 
-const ExistingFacilityReport = ({ index, title, description, setReports }: ExistingFacilityReportProps) => {
-    const [currTitle, setTitle] = useState(title);
-    const [currDescription, setDescription] = useState(description);
-    const [edit, setEdit] = useState(false);
-    
-    const handleCancel = () => {
-        setEdit(() => false);
-        setTitle(() => title);
-        setDescription(() => description);
-    }
+const ExistingFacilityReport = ({
+  index,
+  title,
+  description,
+  setReports,
+}: ExistingFacilityReportProps) => {
+  const [currTitle, setTitle] = useState(title);
+  const [currDescription, setDescription] = useState(description);
+  const [edit, setEdit] = useState(false);
 
-    const handleDelete = (index: number) => {
-        setReports(prevReports => {
-            const splicedReports = [...prevReports];
-            splicedReports.splice(index, 1);
-            console.log(splicedReports);
-            return splicedReports;
-        });
-    }
+  const handleCancel = () => {
+    setEdit(() => false);
+    setTitle(() => title);
+    setDescription(() => description);
+  };
 
-    const handleEdit = () => {
-        setEdit(() => true)
-    }
+  const handleDelete = (index: number) => {
+    setReports((prevReports) => {
+      const splicedReports = [...prevReports];
+      splicedReports.splice(index, 1);
+      console.log(splicedReports);
+      return splicedReports;
+    });
+  };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>, index: number) => {
-        e.preventDefault();
-        setReports(prevReports => {
-            const editedReports = [...prevReports];
-            editedReports[index].title = currTitle;
-            editedReports[index].description = currDescription;
-            return editedReports;
-        })
-        setEdit(() => false);
-    }
+  const handleEdit = () => {
+    setEdit(() => true);
+  };
 
-    return (
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>, index: number) => {
+    e.preventDefault();
+    setReports((prevReports) => {
+      const editedReports = [...prevReports];
+      editedReports[index].title = currTitle;
+      editedReports[index].description = currDescription;
+      return editedReports;
+    });
+    setEdit(() => false);
+  };
+
+  return (
+    <>
+      {!edit ? (
         <>
-            {!edit ? 
-                (
-                    <>
-                        <h2>
-                            {currTitle}
-                        </h2>
-                        <p>
-                            {currDescription}
-                        </p>
-                        <Button onClick={() => handleDelete(index)}>
-                            Delete
-                        </Button>
-                        <Button onClick={handleEdit}>
-                            Edit
-                        </Button>
-                    </>
-                )
-            :
-                (
-                    <form onSubmit={(e) => handleSubmit(e, index)}>
-                        <TextField
-                            label="Title"
-                            name="title"
-                            value={currTitle}
-                            onChange={e => setTitle(e.target.value)}
-                        />
-                        <TextField
-                            label="Description"
-                            name="description"
-                            value={currDescription}
-                            onChange={e => setDescription(e.target.value)}
-                            fullWidth
-                        />
-                        <Button type="button" onClick={handleCancel}>
-                            Cancel
-                        </Button>
-                        <Button type="submit">
-                            Submit
-                        </Button>
-                    </form>
-                )
-            }
+          <h2>{currTitle}</h2>
+          <p>{currDescription}</p>
+          <Button onClick={() => handleDelete(index)}>Delete</Button>
+          <Button onClick={handleEdit}>Edit</Button>
         </>
-    )
-}
+      ) : (
+        <form onSubmit={(e) => handleSubmit(e, index)}>
+          <TextField
+            label="Title"
+            name="title"
+            value={currTitle}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+            label="Description"
+            name="description"
+            value={currDescription}
+            onChange={(e) => setDescription(e.target.value)}
+            fullWidth
+          />
+          <Button type="button" onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button type="submit">Submit</Button>
+        </form>
+      )}
+    </>
+  );
+};
 
 export default memo(ExistingFacilityReport);
--- employee-portal/src/pages/housing_without_backend/HousingDetails.tsx
+++ employee-portal/src/pages/housing_without_backend/HousingDetails.tsx
@@ -1,49 +1,51 @@
-import { Box, Paper } from "@mui/material"
+import { Box, Paper } from '@mui/material';
 
 const house = {
-    address: {
-        num: "{1234}",
-        street: "{Street}",
-        city: "{City}",
-        state: "{State}",
-        zip: "{ZIP}"
+  address: {
+    num: '{1234}',
+    street: '{Street}',
+    city: '{City}',
+    state: '{State}',
+    zip: '{ZIP}',
+  },
+  roommates: {
+    person1: {
+      first_name: 'John',
+      last_name: 'Doe',
+      middle_name: 'Unknown',
+      phone: '(123) 456-7890',
     },
-    roommates: {
-        person1: {
-            first_name: 'John',
-            last_name: 'Doe',
-            middle_name: 'Unknown',
-            phone: '(123) 456-7890'
-        },
-        person2: {
-            first_name: 'Susan',
-            last_name: 'B',
-            middle_name: 'Anthony',
-            preferred_name: 'Jane Doe',
-            phone: '(345) 678-8646'
-        }
-    }
-}
+    person2: {
+      first_name: 'Susan',
+      last_name: 'B',
+      middle_name: 'Anthony',
+      preferred_name: 'Jane Doe',
+      phone: '(345) 678-8646',
+    },
+  },
+};
 
-export default function HousingDetails () {
-    return (
-        <Paper>
-            <Box>
-                <h1>Housing Details</h1>
-            </Box>
-            <Box>
-                <h2>Address</h2>
-                <h3>{Object.values(house.address).join(' ')}</h3>
-            </Box>
-            <Box>
-                <h2>Roommates</h2>
-                {Object.values(house.roommates).map(person => (
-                    <h3 key={person.phone}>
-                        {'preferred_name' in person ? `${person.preferred_name}, ` : `${person.first_name} ${person.middle_name} ${person.last_name}, `}
-                        {person.phone}
-                    </h3>
-                ))}
-            </Box>
-        </Paper>
-    )
+export default function HousingDetails() {
+  return (
+    <Paper>
+      <Box>
+        <h1>Housing Details</h1>
+      </Box>
+      <Box>
+        <h2>Address</h2>
+        <h3>{Object.values(house.address).join(' ')}</h3>
+      </Box>
+      <Box>
+        <h2>Roommates</h2>
+        {Object.values(house.roommates).map((person) => (
+          <h3 key={person.phone}>
+            {'preferred_name' in person
+              ? `${person.preferred_name}, `
+              : `${person.first_name} ${person.middle_name} ${person.last_name}, `}
+            {person.phone}
+          </h3>
+        ))}
+      </Box>
+    </Paper>
+  );
 }
