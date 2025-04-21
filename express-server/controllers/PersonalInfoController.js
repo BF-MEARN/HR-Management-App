d523e5e14c941475e3d484eb453b3e9570628d29
daniellin162 <lindaniel500@gmail.com>
2025-04-21 15:30:51 -0500

    Init controllers for editing documents

---
+++ express-server/controllers/PersonalInfoController.js
@@ -0,0 +1,36 @@
+import Employee from "../models/Employee";
+
+
+/**
+ * GETS
+ */
+
+
+
+
+/**
+ * PUTS
+ */
+export const editName = async (req, res) => {
+
+}
+
+export const editAddress = async (req, res) => {
+
+}
+
+export const editContactInfo = async (req, res) => {
+
+}
+
+export const editEmployment = async (req, res) => {
+
+}
+
+export const editEmergencyContact = async (req, res) => {
+
+}
+
+export const editDocuments = async (req, res) => {
+
+}
