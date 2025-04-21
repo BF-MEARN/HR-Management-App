59884ae2a18391a6f6abf22ffd692c9742c08017
Albert Negoro <albert.negoro89@gmail.com>
2025-04-21 02:32:57 -0700

    HR onboarding app review frontend without view app function

--- express-server/controllers/HrOnboardingController.js
+++ express-server/controllers/HrOnboardingController.js
@@ -1,4 +1,6 @@
 import Employee from '../models/Employee.js';
+// eslint-disable-next-line no-unused-vars
+import VisaStatus from '../models/VisaStatus.js';
 
 /**
  * @desc    Get all pending onboarding applications
---
+++ hr-portal/src/app/pages/hiring-management/components/onboarding-applications/onboarding-applications.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/interfaces/employee';
+import { OnboardingApplicationService } from 'src/app/services/onboarding-application.service';
+
+@Component({
+  selector: 'app-onboarding-applications',
+  templateUrl: './onboarding-applications.component.html',
+  styleUrls: ['./onboarding-applications.component.scss']
+})
+export class OnboardingApplicationsComponent implements OnInit {
+  pending: Employee[] = [];
+  approved: Employee[] = [];
+  rejected: Employee[] = [];
+
+  constructor(private onboardingService: OnboardingApplicationService) { }
+
+  ngOnInit(): void {
+    this.onboardingService.getPending().subscribe((response) => (this.pending = response));
+    this.onboardingService.getApproved().subscribe((response) => (this.approved = response));
+    this.onboardingService.getRejected().subscribe((response) => (this.rejected = response));
+  }
+
+}
