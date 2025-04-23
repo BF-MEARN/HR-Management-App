95a19eca97984f18901b618f5df70a0aa52e69d8
Justin Case <justin.case1061@gmail.com>
2025-04-23 14:23:57 -0500

    fixed route for adding resident to housing

--- express-server/controllers/EmployeeOnboardingController.js
+++ express-server/controllers/EmployeeOnboardingController.js
@@ -2,26 +2,7 @@ import mongoose from 'mongoose';
 
 import Employee from '../models/Employee.js';
 import Housing from '../models/Housing.js';
-import RegistrationToken from '../models/RegistrationToken.js';
-
-export const validateRegistrationToken = async (req, res) => {
-  try {
-    const { tokenUUID } = req.params;
-
-    // not expired, not used
-    const token = await RegistrationToken.findOne({
-      token: tokenUUID,
-      expiresAt: { $gt: new Date() },
-      used: false,
-    });
-    if (!token) {
-      return res.status(404).json({ message: 'Registration token not found or invalid.' });
-    }
-    return res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to validate employee registration token ', error });
-  }
-};
+import VisaStatus from '../models/VisaStatus.js';
 
 export const createNewEmployee = async (req, res) => {
   try {
@@ -100,7 +81,6 @@ export const createHousing = async (req, res) => {
 
     res.status(201).json({ message: `New housing info successfully posted to database!` });
   } catch (error) {
-    console.error('Error posting new housing:', error);
     res.status(500).json({ message: 'Failed to post new housing', error });
   }
 };
@@ -108,7 +88,15 @@ export const createHousing = async (req, res) => {
 export const addResidentToHousing = async (req, res) => {
   try {
     // front end should send address of housing and _id of employee to add
-    const { address, _id } = 'address' in req.body && '_id' in req.body ? req.body : req.autoAssign;
+    const { address, _id } = req.body;
+
+    const residentId = new mongoose.Types.ObjectId(_id);
+
+    let employee = await Employee.findOne({ _id });
+
+    if (!employee) {
+      return res.status(400).json({ message: 'No employee matches the given ID' });
+    }
 
     let existingHousing = await Housing.findOne({
       'address.street': address.street,
@@ -123,14 +111,158 @@ export const addResidentToHousing = async (req, res) => {
         .json({ message: 'No housing matching this address exists in the database' });
     }
 
-    const residentId = new mongoose.Types.ObjectId(_id);
+    employee.houseId = existingHousing._id;
+    employee.address.city = existingHousing.address.city;
+    employee.address.state = existingHousing.address.state;
+    employee.address.street = existingHousing.address.street;
+    employee.address.zip = existingHousing.address.zip;
 
-    existingHousing.residents.push(residentId);
+    await employee.save();
+
+    if (!existingHousing.residents.includes(residentId)) {
+      existingHousing.residents.push(residentId);
+    }
 
     await existingHousing.save();
 
     res.status(200).json({ message: 'Successfully added resident to housing list!' });
   } catch (error) {
     res.status(500).json({ message: 'Failed to post new resident to housing', error });
+  }
+};
+
+export const getEmployeeData = async (req, res) => {
+  try {
+    const { _id } = req.params;
+
+    const employee = await Employee.findOne({ _id });
+
+    if (!employee) {
+      res.status(400).json({ message: 'employee could not be found with given _id' });
+    }
+
+    res.status(200).json({ message: 'Employee data found!', employee });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve employee data', error });
+  }
+};
+
+export const getVisaStatus = async (req, res) => {
+  try {
+    const { _id } = req.params;
+
+    const visaStatus = await VisaStatus.findOne({ employeeId: _id });
+
+    if (!visaStatus) {
+      res.status(400).json({ message: 'Unable to retrieve visa status with given id' });
+    }
+
+    res.status(200).json({ message: 'Visa status retrieved successfully', visaStatus });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve visa status', error });
+  }
+};
+
+export const updateOptReceipt = async (req, res) => {
+  try {
+    const { _id, optReceipt } = req.body;
+
+    const updatedEmployee = await VisaStatus.findOneAndUpdate(
+      { employeeId: _id },
+      {
+        $set: {
+          'optReceipt.file': optReceipt.file,
+          'optReceipt.status': optReceipt.status,
+          'optReceipt.feedback': optReceipt.feedback,
+        },
+      },
+      { new: true } // returns the updated document
+    );
+
+    if (!updatedEmployee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+
+    res.status(200).json({ message: 'OPT Receipt updated successfully', data: updatedEmployee });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to update OPT receipt', error });
+  }
+};
+
+export const updateOptEAD = async (req, res) => {
+  try {
+    const { _id, optEAD } = req.body;
+
+    const updatedEmployee = await VisaStatus.findOneAndUpdate(
+      { employeeId: _id },
+      {
+        $set: {
+          'optEAD.file': optEAD.file,
+          'optEAD.status': optEAD.status,
+          'optEAD.feedback': optEAD.feedback,
+        },
+      },
+      { new: true } // returns the updated document
+    );
+
+    if (!updatedEmployee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+
+    res.status(200).json({ message: 'OPT EAD updated successfully', data: updatedEmployee });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to update OPT EAD', error });
+  }
+};
+
+export const updateI983 = async (req, res) => {
+  try {
+    const { _id, i983 } = req.body;
+
+    const updatedEmployee = await VisaStatus.findOneAndUpdate(
+      { employeeId: _id },
+      {
+        $set: {
+          'i983.file': i983.file,
+          'i983.status': i983.status,
+          'i983.feedback': i983.feedback,
+        },
+      },
+      { new: true } // returns the updated document
+    );
+
+    if (!updatedEmployee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+
+    res.status(200).json({ message: 'I-983 updated successfully', data: updatedEmployee });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to update I-983', error });
+  }
+};
+
+export const updateI20 = async (req, res) => {
+  try {
+    const { _id, i20 } = req.body;
+
+    const updatedEmployee = await VisaStatus.findOneAndUpdate(
+      { employeeId: _id },
+      {
+        $set: {
+          'i20.file': i20.file,
+          'i20.status': i20.status,
+          'i20.feedback': i20.feedback,
+        },
+      },
+      { new: true } // returns the updated document
+    );
+
+    if (!updatedEmployee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+
+    res.status(200).json({ message: 'I-20 updated successfully', data: updatedEmployee });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to update I-20', error });
   }
 };
