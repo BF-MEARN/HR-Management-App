0ad90716cd5ac6bc9f8cd2275342f4e143fca0c8
Albert Negoro <albert.negoro89@gmail.com>
2025-04-25 03:10:03 -0700

    dashboard stuff

--- hr-portal/src/app/pages/home/home.module.ts
+++ hr-portal/src/app/pages/home/home.module.ts
@@ -1,12 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MatCardModule, MatIconModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    RouterModule,
+    // Angular Material modules
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule
+  ],
 })
 export class HomeModule {}
