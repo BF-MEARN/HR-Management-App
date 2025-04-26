879e1737d72dc6826f5f3dc785a54090305d8fd8
Daniel Ho <daniel.ho337@gmail.com>
2025-04-26 00:43:40 -0700

    Remove old HousingPage imports
    Pretty up store.ts

--- employee-portal/src/store/store.ts
+++ employee-portal/src/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import employeeFormReducer from './slices/employeeFormSlice';
+import employeeReducer from './slices/employeeSlice';
 import facilityReportReducer from './slices/facilityReportSlice';
 import housingReducer from './slices/housingSlice';
-import employeeReducer from './slices/employeeSlice';
 import userReducer from './slices/userSlice';
 
 export const store = configureStore({
