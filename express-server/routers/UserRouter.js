23280dd32b6e94597922097d49cad7e345fcf1ad
Albert Negoro <albert.negoro89@gmail.com>
2025-04-25 18:25:18 -0700

    Isolate employee & hr auth

--- express-server/routers/UserRouter.js
+++ express-server/routers/UserRouter.js
@@ -7,15 +7,17 @@ import {
   validateRegistrationToken,
   verifyUser,
 } from '../controllers/UserControllers.js';
-import { userAuth } from '../middlewares/AuthMiddlewares.js';
+import { employeeAuth, hrAuth } from '../middlewares/AuthMiddlewares.js';
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/logout', logout);
+// router.post('/hr-logout', hrLogout);
 router.post('/register', register); //consider use a separated employee register
 router.get('/register/:tokenUUID', validateRegistrationToken);
 
-router.get('/me', userAuth, verifyUser);
+router.get('/me', employeeAuth, verifyUser); // employee-only
+router.get('/hr/me', hrAuth, verifyUser); // HR-only
 
 export default router;
