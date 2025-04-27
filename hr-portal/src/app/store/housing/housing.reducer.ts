6a6cc93cbebd9c1892560a5a6cf3c0a959c66f37
Albert Negoro <albert.negoro89@gmail.com>
2025-04-27 15:50:22 -0700

    HR delete house & reassign

--- hr-portal/src/app/store/housing/housing.reducer.ts
+++ hr-portal/src/app/store/housing/housing.reducer.ts
@@ -34,5 +34,24 @@ export const housingReducer = createReducer(
     ...state,
     error,
     loading: false,
+  })),
+
+  on(HousingActions.deleteHouse, (state) => ({
+    ...state,
+    loading: true,
+    error: null,
+  })),
+
+  on(HousingActions.deleteHouseSuccess, (state, { id }) => ({
+    ...state,
+    houses: state.houses.filter((house) => house._id !== id),
+    loading: false,
+    error: null,
+  })),
+
+  on(HousingActions.deleteHouseFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
   }))
 );
